2a31c610e6c6a5f8ad79dd22e0c118835ab3a4fa
modigliani <fe.villarroelb@gmail.com>
2025-05-08 17:41:11 -0400

    UPDATE : dotenv

--- src/main/java/com/hospital_vm/cl/hospital_vm/HospitalVmApplication.java
+++ src/main/java/com/hospital_vm/cl/hospital_vm/HospitalVmApplication.java
@@ -3,14 +3,12 @@ package com.hospital_vm.cl.hospital_vm;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import io.github.cdimascio.dotenv.Dotenv;
-
 @SpringBootApplication
 public class HospitalVmApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(HospitalVmApplication.class, args);
-		Dotenv dotenv = Dotenv.load();
+		
 
 	}
 
